ccdf2719fe6fa2302abdd4617ab247f6a49866c8
KatlynGreen <batgalkgg@gmail.com>
2022-07-23 13:40:42 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "camuser",
+    password: "Root1234",
+    database: "books_db",
+  },
+  console.log("Connected to the db.")
+);
+
+db.query("select * from favorite_books", (err, results) => {
+  console.log(results);
+  res.json(results);
+});
+
+app.get("/books", (req, res) => {
+  res.json("Some books");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
